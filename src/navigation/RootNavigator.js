6a00a0c16e6d5d6456450bc03b145e24edd4b7d0
brayanwillian-makker <brayan.budal@escola.pr.gov.br>
2026-02-13 12:27:03 -0300

    Created src/navigation/RootNavigator.js file with navigation configuration.

---
+++ src/navigation/RootNavigator.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+const Stack = createStackNavigator();
+
+const RootNavigator = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Details" component={DetailsScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default RootNavigator;
+
+// Dummy components for demonstration
+const HomeScreen = () => <View><Text>Home Screen</Text></View>;
+const DetailsScreen = () => <View><Text>Details Screen</Text></View>;
